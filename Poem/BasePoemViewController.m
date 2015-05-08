78d5e5331c938ec1423d5e9b80dfbd09ab45c262
vade <vade@vade.info>
2015-05-08 16:14:55 -0400

    Added CALayers and remove our UITextView, add fade in, we can now control  each line of text and where it goes procedurally, but whatevers!

--- Poem/BasePoemViewController.m
+++ Poem/BasePoemViewController.m
@@ -11,8 +11,8 @@
 
 @interface BasePoemViewController ()
 @property (atomic, readwrite, strong) Poem* poem;
-@property (atomic, readwrite, strong) IBOutlet UITextView* poemTextView;
-@property (atomic, readwrite, strong) IBOutlet UILabel* poemTextTitleLabel;
+//@property (atomic, readwrite, strong) IBOutlet UITextView* poemTextView;
+//@property (atomic, readwrite, strong) IBOutlet UILabel* poemTextTitleLabel;
 
 @property (atomic, readwrite, strong) IBOutlet UITapGestureRecognizer* tapGestureRecognizer;
 @end
@@ -23,15 +23,15 @@
     [super viewDidLoad];
 
     // Ensure our Gesture Recognizer is hoooked up appropriately.
-    [self.poemTextView addGestureRecognizer:self.tapGestureRecognizer];
+    [self.view addGestureRecognizer:self.tapGestureRecognizer];
     
     // Do any additional setup after loading the view, typically from a nib.
 
     NSURL* poem1URL = [[NSBundle mainBundle] URLForResource:@"Poem1" withExtension:@"rtf"];
     self.poem = [[Poem alloc] initWithPoemFile:poem1URL];
     
-    self.poemTextView.attributedText = nil;
-    self.poemTextTitleLabel.attributedText = self.poem.poemTitle;
+//    self.poemTextView.attributedText = nil;
+//    self.poemTextTitleLabel.attributedText = self.poem.poemTitle;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -45,11 +45,33 @@ static NSUInteger tapCount = 0;
     NSLog(@"Tapped our text view %i", tapCount);
     
     // Copy our Mutable String we have in our poem text
-    NSMutableAttributedString* poemAttributedString = [self.poemTextView.attributedText mutableCopy];
+//    NSMutableAttributedString* poemAttributedString = [self.poemTextView.attributedText mutableCopy];
     
-    [poemAttributedString appendAttributedString:self.poem.attributedLines[tapCount]];
+//    [poemAttributedString appendAttributedString:self.poem.attributedLines[tapCount]];
+//
+//    self.poemTextView.attributedText = poemAttributedString;
+    
+    CABasicAnimation *fadeInAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fadeInAnimation.fromValue = [NSNumber numberWithFloat:0.0];
+    fadeInAnimation.toValue = [NSNumber numberWithFloat:1.0];
+    fadeInAnimation.additive = NO;
+    fadeInAnimation.removedOnCompletion = YES;
+    fadeInAnimation.beginTime = 0.0;
+    fadeInAnimation.duration = 1.0;
+    fadeInAnimation.fillMode = kCAFillModeForwards;
+    
+    CATextLayer* newTextLayer = [CATextLayer layer];
+    newTextLayer.string = self.poem.attributedLines[tapCount];
+    
+    newTextLayer.opacity = 1.0;
+    
+    newTextLayer.frame = CGRectMake(10 , tapCount * 100, 200, 200);
+    
+    
+    [newTextLayer addAnimation:fadeInAnimation forKey:nil];
 
-    self.poemTextView.attributedText = poemAttributedString;
+    [self.view.layer addSublayer:newTextLayer];
+    
     
     tapCount++;
 
